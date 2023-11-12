3e5b4687f837fda01c47a1f30070693c6680055d
Roger Viñas Alcon <roger.vinas@fastzink.com>
2023-11-12 09:42:48 +0100

    Update mockserver (#12)
    * Update mockserver
    
    * Fix version

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ dependencies {
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
   testImplementation("io.mockk:mockk:1.12.4")
   testImplementation("org.assertj:assertj-core:3.22.0")
-  testImplementation("org.mock-server:mockserver-client-java-no-dependencies:5.13.2")
-  testImplementation("org.mock-server:mockserver-junit-jupiter-no-dependencies:5.13.2")
+  testImplementation("org.mock-server:mockserver-client-java-no-dependencies:5.15.0")
+  testImplementation("org.mock-server:mockserver-junit-jupiter-no-dependencies:5.15.0")
   testImplementation("org.testcontainers:testcontainers:1.19.1")
   testImplementation("org.testcontainers:junit-jupiter:1.19.1")
 }
