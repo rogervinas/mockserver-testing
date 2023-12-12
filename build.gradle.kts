24fda6e8aa3805e25e2174a5f4b80c462dd8c0ab
Roger Viñas Alcon <roger.vinas@fastzink.com>
2023-12-12 08:47:33 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,17 +12,20 @@ repositories {
   mavenCentral()
 }
 
+val mockServerVersion = "5.15.0"
+val ktorClientVersion = "2.3.7"
+
 dependencies {
   implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-  implementation("io.ktor:ktor-client-core:2.3.6")
-  implementation("io.ktor:ktor-client-cio:2.3.7")
+  implementation("io.ktor:ktor-client-core:$ktorClientVersion")
+  implementation("io.ktor:ktor-client-cio:$ktorClientVersion")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
   testImplementation("io.mockk:mockk:1.13.8")
   testImplementation("org.assertj:assertj-core:3.24.2")
-  testImplementation("org.mock-server:mockserver-client-java-no-dependencies:5.15.0")
-  testImplementation("org.mock-server:mockserver-junit-jupiter-no-dependencies:5.15.0")
+  testImplementation("org.mock-server:mockserver-client-java-no-dependencies:$mockServerVersion")
+  testImplementation("org.mock-server:mockserver-junit-jupiter-no-dependencies:$mockServerVersion")
   testImplementation(platform("org.testcontainers:testcontainers-bom:1.19.3"))
   testImplementation("org.testcontainers:testcontainers")
   testImplementation("org.testcontainers:junit-jupiter")
