7a172c16045c3af36c0d260a23fe7618d5c216c8
Roger Vinas <roger.vinas@fastzink.com>
2026-05-31 11:52:04 +0200

    Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
+
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
 import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
 import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
 
 plugins {
   id("org.jetbrains.kotlin.jvm") version "2.3.21"
@@ -21,7 +22,6 @@ dependencies {
   implementation("io.ktor:ktor-client-core:$ktorClientVersion")
   implementation("io.ktor:ktor-client-cio:$ktorClientVersion")
 
-  testImplementation(platform("org.junit:junit-bom:6.1.0"))
   testImplementation("org.junit.jupiter:junit-jupiter")
   testImplementation("org.junit.jupiter:junit-jupiter-params")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
